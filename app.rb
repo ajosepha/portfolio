c8170d3016069bdf3e7f56e64516d8d846d957d8
ajosepha <ariel.isaacs@gmail.com>
2013-12-19 01:01:12 -0500

    initial commit, with navajo rugs

---
+++ app.rb
@@ -0,0 +1,21 @@
+require 'bundler'
+Bundler.require
+
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Name
+  class App < Sinatra::Application
+
+    get '/' do
+      
+
+      erb :index
+    end
+
+  
+
+  end
+end
